04135f5704f72f4f992fb412ab644d8d4f3d2d27
henry-makailabs <102751046+henry-makailabs@users.noreply.github.com>
2022-04-04 13:40:57 -0400

    Add color codes (#10)

---
+++ packages/shared/src/colors.ts
@@ -0,0 +1,15 @@
+export const WHITE = '#FFFFFF';
+export const BLACK = '#000000';
+export const BGDARK = '#121212';
+export const PRIMARY = '#00AAE0';
+export const PRIMARYSOLID = '#544EFD';
+export const SECONDARY = '#0067E0';
+export const SUCCESS = '#55C090';
+export const DANGER = '#CD403A';
+export const BLUE200 = '#7CBCFA';
+export const GRAY = '#676767';
+export const GRAY900 = '#0E0E0E';
+export const GRAY800 = '#373737';
+export const GRAY600 = '#747474';
+export const GRAY400 = '#C6C6C6';
+export const GRAY200 = '#E8E8E8';
