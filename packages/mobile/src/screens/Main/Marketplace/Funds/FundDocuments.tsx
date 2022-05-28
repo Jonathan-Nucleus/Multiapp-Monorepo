5e11bd42ca26ece042162e10ddebb2757f48beb2
Darash Desai <102535677+darash-desai@users.noreply.github.com>
2022-05-28 19:52:40 -0400

    Update to build number 37 (#266)
    Co-authored-by: Darash Desai <darash@lyvewave.com>

--- packages/mobile/src/screens/Main/Marketplace/Funds/FundDocuments.tsx
+++ packages/mobile/src/screens/Main/Marketplace/Funds/FundDocuments.tsx
@@ -157,7 +157,7 @@ const styles = StyleSheet.create({
   recentContainer: {
     paddingHorizontal: 16,
     flexDirection: 'row',
-    marginBottom: 16,
+    marginTop: 16,
   },
   recentInfo: {
     flex: 1,
