3d60a39257a7fdf395ac44dced6b9aecfdae49f5
Matthew Bonig <matthew.bonig@gmail.com>
2022-06-02 13:09:05 -0600

    adding healthcheck and dockerfile to video-transcode

--- packages/services/video-transcode/api-routes/routes.js
+++ packages/services/video-transcode/api-routes/routes.js
@@ -4,4 +4,5 @@ const controller = require('../controllers/controller');
 
 module.exports = (app) => {
     app.route('/db-connector').post(controller.dbMediaConvertConnectorController);
+    app.route('/').get((req, res)=>res.json({ message:'Success!' }))
 }
