7939dc8eab6b7806a486bd41c6010ea376b3c2d8
MAK-Adrian <adrian.ghiuta@makailabs.ai>
2022-06-03 20:23:33 +0300

    Add PDF Watermark healthcheck API endpoint.

--- packages/services/pdf-watermark/api-routes/routes.js
+++ packages/services/pdf-watermark/api-routes/routes.js
@@ -4,4 +4,5 @@ const controller = require('../controllers/controller');
 
 module.exports = (app) => {
     app.route('/pdf-watermark').post(controller.pdfWatermarkController);
+    app.route('/').get((req, res)=>res.json({ message:'Success!' }));
 }
