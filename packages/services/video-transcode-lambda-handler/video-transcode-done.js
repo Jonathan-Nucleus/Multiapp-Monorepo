422fc25d567eb8c464d0cd6bde12b8c0a3cedc8a
MAK-Adrian <94563474+MAK-Adrian@users.noreply.github.com>
2022-06-11 21:12:50 +0300

    Services: Update video transcode paths and watermarking server responses on error (#317)
    * Add watchfolder lambda function.
    
    * Add Lambda function that creates MediaConvert jobs.
    
    * Lower code complexity.
    
    * Update AWS Transcode Lambda to use backend API.
    
    * Convert thrown errors to console.error.
    
    * Update prefix structure to cope with single bucket.
    
    * Revert microservice removal.
    
    * Stop watermarking process if watermark or file params are missing or JWT is incorrectly signed.
    
    * Send error response codes on error
    
    * Add 404 response when file is not found
    
    * Update transcoder postId input path index
    
    Co-authored-by: Darash Desai <darash@lyvewave.com>

--- packages/services/video-transcode-lambda-handler/video-transcode-done.js
+++ packages/services/video-transcode-lambda-handler/video-transcode-done.js
@@ -1,65 +1,66 @@
-const https = require('https');
-const requiredEnvVariables = ['DB_CONNECTOR_URL', 'DB_CONNECTOR_PORT', 'API_KEY']
-const dbConnectorURL = process.env.DB_CONNECTOR_URL;
-const dbConnectorPort = process.env.DB_CONNECTOR_PORT;
-const postIdIndex = 2;
-const apiKey = process.env.API_KEY;
-
-let configurationSanityCheck = function() {
-    for (let envVariableIdx in requiredEnvVariables) {
-        if (!(requiredEnvVariables[envVariableIdx] in process.env)) {
-            throw new Error(`Service not configured properly, please provide ${requiredEnvVariables[envVariableIdx]}`);
-        }
+const axios = require("axios").default;
+const jwt = require("jsonwebtoken");
+const postIdIndex = 3;
+const requiredEnvVariables = ["BACKEND_URL", "JWT_SECRET"];
+const AWS = require("aws-sdk");
+const configurationSanityCheck = function () {
+  for (let envVariableIdx in requiredEnvVariables) {
+    if (!(requiredEnvVariables[envVariableIdx] in process.env)) {
+      throw new Error(
+        `Service not configured properly, please provide ${requiredEnvVariables[envVariableIdx]}`
+      );
     }
-}
-
-configurationSanityCheck()
+  }
+};
 
 exports.handler = async (event) => {
-    console.log('event: ', JSON.stringify(event, null, 2));
-    const records = event['Records'];
+  configurationSanityCheck();
 
-    for (let recordIdx in records) {
-        const keyPath = decodeURIComponent(records[recordIdx]['s3']['object']['key']);
-        const keyPathParts = keyPath.split('/');
-        const postId = keyPathParts[postIdIndex];
+  console.log("event: ", JSON.stringify(event, null, 2));
+  const records = event["Records"];
 
-        const body = JSON.stringify({
-            'status': 'Transcoded',
-            'postId': postId,
-            'apiKey': apiKey
-        });
+  let recordIdx = 0;
+  const keyPath = decodeURIComponent(records[recordIdx]["s3"]["object"]["key"]);
+  const keyPathParts = keyPath.split("/");
+  const filename = keyPathParts.slice(-1)[0];
+  const postId = keyPathParts[postIdIndex];
+  const API_URL = process.env.BACKEND_URL;
+  const JWT_SECRET = process.env.JWT_SECRET;
 
-        const options = {
-          hostname: dbConnectorURL,
-          port: dbConnectorPort,
-          path: '/',
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            'Content-Length': body.length,
-          },
-        };
+  const fileExtension = keyPathParts
+    .slice(-1)[0]
+    .split(".")
+    .slice(-1)[0]
+    .toLowerCase();
 
-        const req = https.request(options, res => {
-            if(res.statusCode == 200) {
-                console.log(`Success: postID ${postId}.`);
-            }
-            else {
-                console.error(`Failed: Received status code ${res.statusCode} for postID ${postId}.`);
-            }
-        });
-
-        req.on('error', error => {
-          console.error(error);
-        });
-
-        req.write(body);
-        req.end();
-    }
-
-    const response = {
-        statusCode: 200
+  if (["mp4", "mov", "avi"].includes(fileExtension)) {
+    const payload = {
+      postId: postId,
+      mediaUrl: filename,
     };
-    return response;
+    const token = jwt.sign(payload, JWT_SECRET);
+    try {
+      const res = await axios.post(API_URL, {
+        query: `
+          mutation UpdatePostMedia($token: String!) {
+            updatePostMedia(token: $token)
+          }
+        `,
+        variables: {
+          token: token,
+        },
+      });
+      if (res.status == 200) {
+        console.log(res);
+      } else {
+        console.error(res);
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  return {
+    status: 200,
+  };
 };
