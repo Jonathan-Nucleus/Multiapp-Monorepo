50234fed0de064251c15e772a8b52024cd1ae285
Darash Desai <darash@lyvewave.com>
2022-06-12 17:07:22 -0400

    Backend: Update S3 bucket env var name

--- packages/backend/lib/s3-helper.ts
+++ packages/backend/lib/s3-helper.ts
@@ -20,7 +20,7 @@ const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
 const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
 const AWS_UPLOAD_REGION = process.env.AWS_REGION;
 const MRAP_ENDPOINT = process.env.MRAP_ENDPOINT;
-const S3_BUCKET = process.env.S3_BUCKET;
+const S3_BUCKET = process.env.AWS_S3_BUCKET;
 
 const VIDEO_EXTS = ["mp4", "mov", "avi"];
 
@@ -74,6 +74,7 @@ export async function getUploadUrl(
   // endpoint
 
   if (!AWS_UPLOAD_REGION || !MRAP_ENDPOINT) {
+    console.error("Missing AWS configuration");
     throw new InternalServerError("Missing AWS configuration");
   }
 
@@ -124,6 +125,7 @@ export async function movePostMedia(
   }
 
   if (!AWS_UPLOAD_REGION || !S3_BUCKET) {
+    console.error("Missing AWS configuration");
     throw new InternalServerError("Missing AWS configuration");
   }
 
