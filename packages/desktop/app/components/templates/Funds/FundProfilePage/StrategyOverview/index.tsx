53664f917ad02f8679a5634371b74c7e9dd3b015
MAK-Denis <denis.charnysh@makailabs.ai>
2022-07-11 20:07:36 +0200

    desktop: Removed the highlights & metrics from the strategy overview

--- packages/desktop/app/components/templates/Funds/FundProfilePage/StrategyOverview/index.tsx
+++ packages/desktop/app/components/templates/Funds/FundProfilePage/StrategyOverview/index.tsx
@@ -14,14 +14,11 @@ import localData from "dayjs/plugin/localeData";
 import Accordion from "../../../../common/Accordion";
 import ManagerCard from "../ManagerCard";
 import DetailedTeamMemberList from "../DetailedTeamMemberList";
-import TeamMembersList from "../../../../modules/teams/TeamMembersList";
 import DisclosureModal from "../../../../modules/funds/DisclosureModal";
 import FundDocuments from "../FundDocuments";
 
 dayjs.extend(localData);
 dayjs.extend(utc);
-
-const MONTHS = dayjs().localeData().monthsShort();
 
 interface StrategyOverviewProps {
   fund: FundDetails;
@@ -31,17 +28,6 @@ const StrategyOverview: FC<StrategyOverviewProps> = ({ fund }) => {
   const { isWatching, toggleWatch } = useWatchFund(fund._id);
   const [showDisclosureModal, setShowDisclosureModal] = useState(false);
   const fundTeam = [fund.manager, ...(fund?.team ?? [])];
-  const sortedReturns = fund.metrics.sort(
-    (a, b) => a.date.getTime() - b.date.getTime()
-  );
-  const earliestYear = sortedReturns[0]?.date.getFullYear();
-  // Assume last entry is YTD for last year
-  const latestYear =
-    sortedReturns[sortedReturns.length - 2]?.date.getFullYear();
-  const numYears = latestYear - earliestYear + 1;
-  const years = [...Array(!isNaN(numYears) ? numYears : 0)]
-    .map((_, index) => earliestYear + index)
-    .reverse();
 
   return (
     <div className="mt-5 px-2 flex justify-center">
@@ -149,15 +135,15 @@ const StrategyOverview: FC<StrategyOverviewProps> = ({ fund }) => {
                       </ReactMarkdown>
                     </div>
                   </div>
-                  {fund.presentationUrl ? (
+                  {fund.presentationUrl && (
                     <div className="mt-5 mb-6">
                       <Button variant="outline-primary">
                         <TelevisionSimple color="currentColor" size={24} />
                         <span className="ml-3">View Presentation</span>
                       </Button>
                     </div>
-                  ) : null}
-                  <div className="mt-6">
+                  )}
+                  <div className="border-b border-white/[.12] mt-6 pb-6">
                     <div
                       className={`flex flex-wrap -mx-1 py-2 text-xs text-white/[.60]
                       font-medium rounded-full uppercase m-1 px-1 py-1
@@ -166,99 +152,17 @@ const StrategyOverview: FC<StrategyOverviewProps> = ({ fund }) => {
                       {fund.tags.join(" • ")}
                     </div>
                   </div>
-                  <div className="border-t border-white/[.12] mt-4" />
-                  <div className="mt-6">
-                    <div className="text-xl text-white font-medium items-center">
-                      Highlights
-                      <span className="float-right text-white/[0.6] text-xs leading-7 font-light">
-                        As of {dayjs(fund.updatedAt).utc().format("M/DD/YYYY")}
-                      </span>
-                    </div>
-                    <Card className="mt-5 p-0">
-                      <div className="flex border-white/[.12] divide-x divide-inherit">
-                        {fund.attributes.map((attribute, index) => (
-                          <div
-                            key={index}
-                            className="flex flex-1 flex-col items-center py-6"
-                          >
-                            <div className="text-tiny text-white/[.6] text-center uppercase">
-                              {attribute.label}
-                            </div>
-                            <div className="text-sm text-white mt-1">
-                              {attribute.value}
-                            </div>
-                          </div>
-                        ))}
-                      </div>
-                    </Card>
-                  </div>
-                  {!fund.limitedView && fund.metrics.length > 0 ? (
-                    <div className="mt-6 text-white/[0.3]">
-                      <div className="mb-2 font-light tracking-widest">
-                        Monthly Net Return
-                      </div>
-                      <table className="w-full border-collapse border border-white/[.12] text-[0.75rem]">
-                        <thead className="font-bold">
-                          <tr>
-                            <th className="border border-white/[.12] py-3" />
-                            {MONTHS.map((month) => (
-                              <th
-                                key={month}
-                                className="border border-white/[.12] py-3 text-white/[0.4]"
-                              >
-                                {month}
-                              </th>
-                            ))}
-                            <th className="border border-white/[.12] py-3 text-white/[0.4]">
-                              YTD
-                            </th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          {years.map((year) => {
-                            const ytdFigure = sortedReturns.find(
-                              (metric) =>
-                                metric.date.getUTCMonth() === 0 &&
-                                metric.date.getUTCFullYear() === year + 1 &&
-                                metric.date.getUTCDate() === 1
-                            )?.figure;
-
-                            return (
-                              <tr key={year}>
-                                <td className="font-bold border border-white/[.12] py-3 text-center text-white/[0.4]">
-                                  {year}
-                                </td>
-                                {MONTHS.map((month, index) => {
-                                  const figure = sortedReturns.find(
-                                    (metric) =>
-                                      metric.date.getUTCMonth() === index &&
-                                      metric.date.getUTCFullYear() === year &&
-                                      metric.date.getUTCDate() !== 1
-                                  )?.figure;
-
-                                  return (
-                                    <td
-                                      key={month}
-                                      className="text-center border border-white/[.12] py-3"
-                                    >
-                                      {figure !== undefined
-                                        ? `${figure.toFixed(1)}%`
-                                        : "--"}
-                                    </td>
-                                  );
-                                })}
-                                <td className="text-center font-bold border border-white/[.12] py-3 text-white/[0.4]">
-                                  {ytdFigure !== undefined
-                                    ? `${ytdFigure.toFixed(1)}%`
-                                    : "--"}
-                                </td>
-                              </tr>
-                            );
-                          })}
-                        </tbody>
-                      </table>
-                    </div>
-                  ) : null}
+                  {fund.disclosure && (
+                    <Accordion
+                      title="Fund Disclosures"
+                      titleClassName="text-sm font-medium tracking-wider"
+                      className="mt-5"
+                    >
+                      <p className="text-xs leading-5 text-white/[0.4] whitespace-pre-line">
+                        {fund.disclosure}
+                      </p>
+                    </Accordion>
+                  )}
                 </Tab.Panel>
                 <Tab.Panel>
                   {fund.documents && (
@@ -272,17 +176,6 @@ const StrategyOverview: FC<StrategyOverviewProps> = ({ fund }) => {
                 </Tab.Panel>
               </Tab.Panels>
             </Tab.Group>
-            {fund.disclosure && (
-              <Accordion
-                title="Fund Disclosures"
-                titleClassName="text-sm font-medium tracking-wider"
-                className="mt-8"
-              >
-                <p className="text-xs leading-5 text-white/[0.4] whitespace-pre-line">
-                  {fund.disclosure}
-                </p>
-              </Accordion>
-            )}
             <div className="border-t border-white/[.12] mt-6 mb-24 pt-2">
               <Button
                 variant="text"
@@ -298,11 +191,7 @@ const StrategyOverview: FC<StrategyOverviewProps> = ({ fund }) => {
         <div className="lg:ml-8">
           <ManagerCard fund={fund} />
           <div className="mt-9 px-3">
-            {fund.limitedView ? (
-              <DetailedTeamMemberList members={fundTeam} />
-            ) : (
-              <TeamMembersList members={fundTeam} />
-            )}
+            <DetailedTeamMemberList members={fundTeam} />
           </div>
         </div>
       </div>
