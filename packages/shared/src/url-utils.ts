7f87dc3a2d81aef4d87bfa53d81ad1a5b27f021d
osamaMkai <109529954+osamaMkai@users.noreply.github.com>
2022-07-27 03:24:17 +0500

    Mobile: Fixed crash related to embedded youtube videos (#447)
    * Fixed crash in youtube embed
    
    * Remove extra comments
    
    * Add return types
    
    Co-authored-by: OsamaMukhtar <osamamukhtar89@gmail.com>
    Co-authored-by: Darash Desai <darash.desai@makailabs.ai>

--- packages/shared/src/url-utils.ts
+++ packages/shared/src/url-utils.ts
@@ -1,16 +1,12 @@
-export const getVideoIdFromYoutubeLink = (url: string) => {
-  var video_id = url.split("v=")[1];
-  var ampersandPosition = video_id.indexOf("&");
-  if (ampersandPosition != -1) {
-    video_id = video_id.substring(0, ampersandPosition);
-  }
-  return video_id;
+export const getVideoIdFromYoutubeLink = (url: string): string | undefined => {
+  const idPattern =
+    /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
+  const match = url.match(idPattern);
+  return match && match[2].length === 11 ? match[2] : undefined;
 };
-export const isValidYoutubeUrl = (url: string) => {
-  var p =
+
+export const isValidYoutubeUrl = (url: string): boolean => {
+  const urlPattern =
     /^(?:https?:\/\/)?(?:m\.|www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/;
-  if (url.trim().match(p)) {
-    return true;
-  }
-  return false;
+  return !!url.trim().match(urlPattern);
 };
