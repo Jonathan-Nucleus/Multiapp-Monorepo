81cc07271f3dd4467e9523f482ddc7c33d55b781
Darash Desai <102535677+darash-desai@users.noreply.github.com>
2022-07-29 22:47:28 -0400

    Mobile: Fix text collapse bug for mentions (#469)

--- packages/mobile/src/components/common/PBodyText.tsx
+++ packages/mobile/src/components/common/PBodyText.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useCallback, useMemo, useRef, useState } from 'react';
+import React, { FC, useCallback, useMemo, useState } from 'react';
 import {
   View,
   StyleSheet,
@@ -6,7 +6,6 @@ import {
   StyleProp,
   TextStyle,
   TextLayoutEventData,
-  Dimensions,
 } from 'react-native';
 
 import * as NavigationService from 'mobile/src/services/navigation/NavigationService';
@@ -14,9 +13,6 @@ import { WHITE, PRIMARY } from 'shared/src/colors';
 
 import { processPost, TAG_PATTERN } from 'shared/src/patterns';
 import PText from './PText';
-
-const INITIAL_READ_MORE_WIDTH = 86;
-const INITIAL_CONTAINER_WIDTH = Dimensions.get('window').width - 32;
 
 interface PBodyTextProps {
   body?: string;
@@ -34,16 +30,8 @@ const PBodyText: FC<PBodyTextProps> = ({
 }) => {
   // Remove last line break from body;
   const initialBody = body?.replace(/\n$/, '') ?? '';
-  const [showingBody, setShowingBody] = useState(initialBody);
-  const [isCollapsed, setIsCollapsed] = useState(true);
-  const [collapsedText, setCollapsedText] = useState(initialBody);
-  const [more, setMore] = useState(true);
-  const [initialLoaded, setInitialLoaded] = useState(false);
-  const [initialBtnLoaded, setInitialBtnLoaded] = useState(false);
-  const [containerWidth, setContainerWidth] = useState(INITIAL_CONTAINER_WIDTH);
-  const [btnTextWidth, setBtnTextWidth] = useState(INITIAL_READ_MORE_WIDTH);
-  const lastLine = useRef<number>(0);
-  const limitLine = useRef<number>(0);
+  const [showMore, setShowMore] = useState(false);
+  const [collapsed, setCollapsed] = useState(true);
 
   const goToUrl = (url: string): void => {
     let link = url;
@@ -70,85 +58,26 @@ const PBodyText: FC<PBodyTextProps> = ({
 
   const onHiddenTextLayout = useCallback(
     (e: { nativeEvent: TextLayoutEventData }) => {
-      if (!initialLoaded && collapseLongText) {
-        const { lines: txtLines = [] } = e.nativeEvent as TextLayoutEventData;
-        const showMore = txtLines.length > numberOfLines;
-        setMore(showMore);
-        if (showMore) {
-          const showingLines = txtLines
-            .slice(0, numberOfLines)
-            .map(({ text }, index) => {
-              let ret = '';
-              if (index < numberOfLines - 1) {
-                ret = text;
-              } else {
-                // Remove enter key of last line
-                ret = text.replace(/(\r\n|\n|\r)/gm, '');
-              }
-
-              return ret;
-            });
-
-          setShowingBody(showingLines.join('') ?? '');
-          setCollapsedText(showingLines.join('') ?? '');
-        }
-        setInitialLoaded(true);
-        lastLine.current = txtLines[txtLines.length - 1].width;
-        limitLine.current =
-          txtLines.length > numberOfLines
-            ? txtLines[numberOfLines - 1].width
-            : lastLine.current;
+      if (!collapseLongText || numberOfLines <= 0) {
+        return;
       }
-    },
-    [collapseLongText, initialLoaded, numberOfLines],
-  );
 
-  const onHiddenButtonTextLayout = useCallback(
-    (e: { nativeEvent: TextLayoutEventData }) => {
       const { lines: txtLines = [] } = e.nativeEvent as TextLayoutEventData;
-      const length = txtLines.length;
-
-      if (!initialBtnLoaded && collapseLongText) {
-        if (length > 0) {
-          setBtnTextWidth(txtLines[length - 1].width);
-          setInitialBtnLoaded(true);
-        }
-      }
+      const _showMore = txtLines.length > numberOfLines;
+      setShowMore(_showMore);
     },
-    [collapseLongText, initialBtnLoaded],
+    [collapseLongText, numberOfLines],
   );
 
-  const onContainerLayout = useCallback(
-    (e: { nativeEvent: { layout: { width: number } } }) => {
-      const { width } = e.nativeEvent.layout;
-      setContainerWidth(width);
-    },
-    [],
-  );
+  const handleMoreText = (): void => {
+    setCollapsed(!collapsed);
+  };
 
-  const handleMoreText = useCallback(() => {
-    if (initialBody) {
-      if (isCollapsed === true) {
-        setShowingBody(initialBody);
-      } else {
-        setShowingBody(collapsedText);
-      }
-      setIsCollapsed(!isCollapsed);
-    }
-  }, [collapsedText, initialBody, isCollapsed]);
-
-  const readMoreText = useMemo(() => {
-    const determinedLine =
-      (isCollapsed ? limitLine.current : lastLine.current) + btnTextWidth >=
-      containerWidth;
-    return `${determinedLine ? '\n' : ' '}Read ${
-      isCollapsed ? 'more...' : 'less'
-    }`;
-  }, [btnTextWidth, containerWidth, isCollapsed]);
+  const readMoreText =
+    collapseLongText && collapsed ? 'Read more...' : 'Read less';
 
   const renderText = useMemo(() => {
-    const val = !collapseLongText ? initialBody : showingBody;
-    return processPost(val).map((split, index) => {
+    return processPost(initialBody).map((split, index) => {
       if (
         (split.startsWith('$') || split.startsWith('#')) &&
         split.match(TAG_PATTERN)
@@ -163,21 +92,25 @@ const PBodyText: FC<PBodyTextProps> = ({
         );
       } else if (split.startsWith('@') && split.includes('|')) {
         const [name, id] = split.substring(1).split('|');
+        const text = `@${name}`;
+
         return (
           <PText
             key={id}
             style={styles.tagLink}
             onPress={() => goToProfile(id)}>
-            @{name}
+            {text}
           </PText>
         );
       } else if (split.startsWith('%%')) {
+        const text = split.substring(2);
+
         return (
           <PText
             key={`${split}-${index}`}
             style={styles.tagLink}
             onPress={() => goToUrl(split.substring(2).trim())}>
-            {split.substring(2)}
+            {text}
           </PText>
         );
       } else {
@@ -186,36 +119,52 @@ const PBodyText: FC<PBodyTextProps> = ({
         );
       }
     });
-  }, [initialBody, collapseLongText, showingBody]);
+  }, [initialBody]);
 
   if (!initialBody) {
     return null;
   }
 
   return (
-    <View style={styles.previewContainer} onLayout={onContainerLayout}>
+    <View style={styles.previewContainer}>
       {collapseLongText && (
         <PText
           style={[styles.body, styles.hidden, style]}
           onTextLayout={onHiddenTextLayout}>
-          {initialBody}
+          {renderText}
         </PText>
       )}
-      {collapseLongText && (
+      <View
+        style={[
+          styles.collapseWrapper,
+          {
+            ...(showMore && collapsed
+              ? {
+                  height: styles.body.lineHeight * Math.max(1, numberOfLines),
+                }
+              : {}),
+          },
+        ]}>
         <PText
-          style={[styles.body, styles.hidden, style]}
-          onTextLayout={onHiddenButtonTextLayout}>
-          {readMoreText + ' '}
+          style={[
+            styles.body,
+            style,
+            { ...(showMore && collapsed ? { position: 'absolute' } : {}) },
+          ]}
+          selectable={true}>
+          {renderText}
+          {collapseLongText && showMore && !collapsed ? (
+            <PText style={styles.more} onPress={handleMoreText}>
+              {readMoreText}
+            </PText>
+          ) : null}
         </PText>
-      )}
-      <PText style={[styles.body, style]} selectable={true}>
-        {!collapseLongText ? renderText : initialBtnLoaded ? renderText : ''}
-        {initialBtnLoaded && collapseLongText && more === true ? (
-          <PText style={styles.more} onPress={handleMoreText}>
-            {readMoreText}
-          </PText>
-        ) : null}
-      </PText>
+      </View>
+      {collapseLongText && showMore && collapsed ? (
+        <PText style={styles.more} onPress={handleMoreText}>
+          {readMoreText}
+        </PText>
+      ) : null}
     </View>
   );
 };
@@ -224,6 +173,9 @@ export default PBodyText;
 
 const styles = StyleSheet.create({
   previewContainer: {
+    paddingBottom: 16,
+  },
+  collapseWrapper: {
     overflow: 'hidden',
   },
   body: {
@@ -235,6 +187,8 @@ const styles = StyleSheet.create({
   },
   more: {
     color: PRIMARY,
+    lineHeight: 20,
+    flexWrap: 'nowrap',
   },
   hidden: {
     position: 'absolute',
