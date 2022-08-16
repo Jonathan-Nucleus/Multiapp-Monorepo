7d670ff1362e4c8fc565cfc7f4a6c61c0855b4b2
Darash Desai <102535677+darash-desai@users.noreply.github.com>
2022-08-16 12:33:33 -0400

    Database: Migrate media field to an array (#505)
    * Migrate media field to an array
    
    * Update migration script

---
+++ packages/backend/migrations/20220811222646-multiple-posts.js
@@ -0,0 +1,17 @@
+module.exports = {
+  async up(db) {
+    await db.collection("posts").updateMany(
+      { media: { $exists: true } },
+      [{ $set: { media: ["$media"] } }],
+      { multi: true }
+    )
+  },
+
+  async down(db) {
+    await db.collection("posts").updateMany(
+      { media: { $exists: true } },
+      [{ $set: { media: { $first: "$media" } } }],
+      { multi: true }
+    )
+  }
+};
